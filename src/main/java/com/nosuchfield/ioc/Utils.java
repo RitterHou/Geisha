2bb0ce7b9090ce57c191101ec2b33582107ace3c
derobukal <derobukal@gmail.com>
2017-10-19 23:46:50 +0800

    支持windows路径格式

--- src/main/java/com/nosuchfield/ioc/Utils.java
+++ src/main/java/com/nosuchfield/ioc/Utils.java
@@ -13,6 +13,9 @@ public class Utils {
     private static List<Class> classes;
 
     public static List<Class> getAllClass() {
+        root = new File(root).getPath().replaceAll("\\\\", "/"); // 因为不同操作系统的路径格式不一致，此操作旨在使路径格式一致
+        if (!root.endsWith("/"))
+            root = root + "/";
         classes = new ArrayList<>();
         listFilesForFolder(new File(root));
         return classes;
@@ -23,7 +26,7 @@ public class Utils {
             if (file.isDirectory()) {
                 listFilesForFolder(file);
             } else {
-                String path = file.getPath();
+                String path = file.getPath().replaceAll("\\\\", "/");
                 if (path.endsWith(".class")) {
                     String classPath = path.replaceAll(root, "").replaceAll("/", ".").replaceAll(".class", "");
                     try {
