45166606915c4e01cc80cb692d96468e6d90e985
derobukal <derobukal@gmail.com>
2017-10-25 21:43:58 +0800

    优化获取包名的方式

--- src/main/java/com/nosuchfield/geisha/Utils.java
+++ src/main/java/com/nosuchfield/geisha/Utils.java
@@ -3,17 +3,19 @@ package com.nosuchfield.geisha;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 /**
  * @author hourui 2017/10/18 19:37
  */
 public class Utils {
 
-    private static String root = Utils.class.getClassLoader().getResource("").getPath();
-    private static String separator = File.separator;
+    private static String root = Utils.class.getClassLoader().getResource("").getPath(); // CLASSPATH
+    private static String separator = File.separator; // 分隔符
     private static List<Class> classes;
 
     public static List<Class> getAllClass() {
+        root = new File(root).getPath(); // 更换CLASSPATH的样式
         classes = new ArrayList<>();
         listFilesForFolder(new File(root));
         return classes;
@@ -26,9 +28,12 @@ public class Utils {
             } else {
                 String path = file.getPath();
                 if (path.endsWith(".class")) {
-                    String classPath = path.replaceAll(root, "")
-                            .replaceAll(separator, ".")
-                            .replaceAll(".class", "");
+                    String classPath = path.replaceAll(Matcher.quoteReplacement(root), "") // 去掉绝对路径
+                            .replaceAll(Matcher.quoteReplacement(separator), ".") // 更换间隔符
+                            .replaceAll(".class", ""); // 去掉后缀
+
+                    if (classPath.startsWith(Matcher.quoteReplacement("."))) // 去掉开头的 .
+                        classPath = classPath.replaceFirst(Matcher.quoteReplacement("."), "");
                     try {
                         classes.add(Class.forName(classPath));
                     } catch (ClassNotFoundException e) {
